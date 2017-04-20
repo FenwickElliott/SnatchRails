a84b7b0b50a38e4d2a642f240404ffda8a287760
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2017-04-20 15:06:04 -0400

    api calls stable

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,16 +1,22 @@
 class Api::V1::UsersController < Api::V1::BaseController
 
+  require 'json'
+  require 'net/http'
+  require 'uri'
+
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
   def show
-    render(json: {:name=>"Charles"}.to_json)
+    render(json: {:method=>"show"}.to_json, status: 201)
   end
 
   def index
-    render(json: {:name=>"Charlie"}.to_json)
+    puts "Hello"
+    render(json: {:method=>"index"}.to_json, status: 201)
   end
 
   def not_found
     return api_error(status: 404, errors: 'Not found')
   end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   before_create :initiate_settings
+  before_create :generate_auth_token
 
   def initiate_settings
     self.settings ||= {p_name: "Snatched"}.to_json
+  end
+
+  def generate_auth_token
+    loop do
+      self.auth_token = SecureRandom.base64(64)
+      break unless User.find_by(auth_token: auth_token)
+    end
   end
 end
