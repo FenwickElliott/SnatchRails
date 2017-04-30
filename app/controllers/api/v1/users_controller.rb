df58ebf8b66cac56b39d25e4ed9fb08934feee04
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2017-04-30 14:15:36 -0400

    create_playlist implimented

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,32 +4,66 @@ class Api::V1::UsersController < Api::V1::BaseController
 
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
+  def snatch
+    get_user_info
+    get_song
+    check_for_playlist
+  end
+
+  def get_user_info
+    @user_id = get('me')['id']
+    @ans << "Got @user_id: #{@user_id}\n"
+
+    # two line implemental runs faster than one line implimentation. Why?
+    settings = JSON.parse User.find(params[:id]).settings
+    @p_name = settings['p_name']
+    # p_name = JSON.parse(User.find(params[:id]).settings)['p_name']
+    @ans << "Got p_name: #{@p_name}\n"
+  end
+
+  def get_song
+    s_uri = get('me/player/currently-playing')['item']['uri']
+    s_name = get('me/player/currently-playing')['item']['name']
+    @ans << "Got song: #{s_name}\n"
+  end
+
+  def check_for_playlist
+    list = get('me/playlists?limit=50')
+    list['items'].each do |x|
+    # get('me/playlists?limit=50')'items'].each do |x|
+      if x['name'] === @p_name
+        p_id = x['id']
+        @ans << "Playlist found, p_id: #{p_id}\n"
+        return
+      end
+    end
+    @ans << "check_for_playlist complete, playlist not found. Creating...\n"
+    create_playlist
+  end
+
+  def create_playlist
+    playlist = post("users/#{@user_id}/playlists", {
+      "description" => "Your Snatched Playlist",
+      "public" => false,
+      "name" => @p_name
+    })
+    @p_id = playlist['id']
+    @ans << "create_playlist complete #{@p_name} playlist created. ID: #{@p_id}\n"
+  end
+
   def show
-    @user = User.find(params[:id])
     @ans = ""
+    @user = User.find(params[:id])
     @access_token = User.find(params[:id]).access_token
-    @refresh_token = User.find(params[:id]).refresh_token
-    @ans << "Got token\n"
-
     begin
-      user_id = get('me')['id']
-      @ans << "Got user_id: #{user_id}\n"
-
-      s_uri = get('me/player/currently-playing')['item']['uri']
-      s_name = get('me/player/currently-playing')['item']['name']
-      @ans << "Got song: #{s_name}\n"
-
-
-      # two line implemental runs faster than one line implimentation. Why?
-      settings = JSON.parse User.find(params[:id]).settings
-      @p_name = settings['p_name']
-      # p_name = JSON.parse(User.find(params[:id]).settings)['p_name']
-      @ans << "Got p_name: #{@p_name}\n"
-
-      check_for_playlist
+      @ans << "Got token\n"
+      snatch
       render json: @ans
     rescue
+      @ans << "Token Refreshed"
       cycle_tokens
+      snatch
+      render json: @ans
     end
   end
 
@@ -47,37 +81,18 @@ class Api::V1::UsersController < Api::V1::BaseController
     request["Authorization"] = "Basic ZWJkNTM1NDVlZTA0NGI3OGE2MTc5OTk4ZjBkZGZiNDA6MjIzMjY3ZmNmMjI0NGJhYWI1OTIwMDVjMzI4Y2E2Y2U="
     request.set_form_data(
       "grant_type" => "refresh_token",
-      "refresh_token" => @refresh_token,
+      "refresh_token" => User.find(params[:id]).refresh_token
     )
     req_options = {
       use_ssl: uri.scheme == "https",
     }
-
     response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
       http.request(request)
     end
-
     @access_token = JSON.parse(response.body)['access_token']
-
     @user.access_token = @access_token
     @user.save!
-
-    render json: response
   end
-
-  def check_for_playlist
-      list = get('me/playlists?limit=50')
-      list['items'].each do |x|
-        if x['name'] === @p_name
-          p_id = x['id']
-          @ans << "Playlist found, p_id: #{p_id}\n"
-          return
-        end
-      end
-      @ans << "check_for_playlist complete, playlist not found. Creating...\n"
-      # create_playlist
-  end
-
 
   def get(endpoint)
     uri = URI.parse("https://api.spotify.com/v1/#{endpoint}")
@@ -99,7 +114,7 @@ class Api::V1::UsersController < Api::V1::BaseController
     request = Net::HTTP::Post.new(uri)
     request.content_type = "application/json"
     request["Accept"] = "application/json"
-    request["Authorization"] = "Bearer #{session[:token]}"
+    request["Authorization"] = "Bearer #{@access_token}"
     request.body = JSON.dump body
     req_options = {
       use_ssl: uri.scheme == "https",
