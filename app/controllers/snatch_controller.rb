bceea75735645cfb880c373399314c32552e4c66
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2017-05-03 16:49:12 -0400

    Agressive refactoring, guest_snatch established

--- app/controllers/snatch_controller.rb
+++ app/controllers/snatch_controller.rb
@@ -2,31 +2,39 @@ class SnatchController < ApplicationController
   require 'json'
   require 'net/http'
 
-  def about
+  def link
     if request.env['omniauth.auth']
-      session[:response] = request.env['omniauth.auth']
-      session[:token] = session[:response][:credentials][:token]
-      session[:header] = {
-        Accept: "application/json",
-        Authorization: "Authorization: Bearer #{session[:token]}"
-      }
-      get_me
       if current_user
-        session[:p_name] = JSON.parse(current_user.settings)["p_name"]
-        current_user.access_token = session[:token]
-        current_user.refresh_token = session[:response][:credentials][:refresh_token]
+        current_user.access_token = request.env['omniauth.auth'][:credentials][:token]
+        current_user.refresh_token = request.env['omniauth.auth'][:credentials][:refresh_token]
+        p_set = JSON.parse(current_user.settings)
+        p_set["user_id"] = get('me')["id"]
+        current_user.settings = p_set.to_json
         current_user.save!
       else
-        session[:p_name] = "Snatched"
+        response = request.env['omniauth.auth']
+        session[:token] = response[:credentials][:token]
       end
-      if session[:que]
-        session[:que] = false
-        redirect_to snatch_path
-      end
+    end
+    redirect_to root_path
+  end
+
+  def about
+    if current_user && !current_user.access_token 
+      redirect_to "/auth/spotify"
+    end
+    if session[:que] == true
+      session[:que] = false
+      guest_snatch
     end
   end
 
   def options
+  end
+
+  def fail
+    flash[:alert] = "Rejected"
+    redirect_to root_path
   end
 
   def update
@@ -42,53 +50,56 @@ class SnatchController < ApplicationController
   end
 
   def snatch
-    unless session[:user_id]
-      session[:que] = true
-      redirect_to "/auth/spotify"
-    end
     begin
+      session[:user_id] = get('me')['id']
       get_song
       check_for_playlist
-      check_through_playlist
-      # actually_snatch
-      redirect_to root_path
+      actually_snatch
     rescue
+      puts "Rescued"
     end
+    redirect_to root_path
   end
 
-  def get_me
-    begin
-      user = get('me')
-      session[:user_id] = user['id']
-      puts "got me: #{session[:user_id]}"
-    rescue
-      puts "couldn't access spotify api"
-      flash[:alert] = "I'm sorry, we couldn't access the Spotify API, which is problematic..."
+  def guest_snatch
+    if session[:token]
+      session[:user_id] = get('me')['id']
+      get_song
+      check_for_playlist
+      actually_snatch
+      redirect_to root_path
+    else
+      session[:que] = true
+      redirect_to "/auth/spotify"
     end
   end
 
   def get_song
-    song = get('me/player/currently-playing')
-    session[:s_uri] = song['item']['uri']
-    session[:s_name] = song['item']['name']
-    puts "get_song complete, got #{session[:s_name]}"
+    begin
+      song = get('me/player/currently-playing')
+      session[:s_uri] = song['item']['uri']
+      session[:s_name] = song['item']['name']
+      puts "get_song complete, got #{session[:s_name]}"
+    rescue
+      flash[:alert] = "Could not get current song. Are you sure one is playing?"
+    end
   end
 
   def check_for_playlist
-    # if true
-      list = get('me/playlists?limit=50')
-      list['items'].each do |x|
-          if x['name'] === session[:p_name]
-
-            puts x['name'] << ' Playlist found'
-            session[:p_id] = x['id']
-            return
-          end
+    if current_user
+      session[:p_name] = JSON.parse(current_user.settings)["p_name"]
+    else
+      session[:p_name] = "Snatched"
+    end
+    get('me/playlists?limit=50')['items'].each do |x|
+        if x['name'] === session[:p_name]
+          puts x['name'] << ' Playlist found'
+          session[:p_id] = x['id']
+          return
         end
-        puts "check_for_playlist complete, playlist not found, creating"
-        create_playlist
-      # end
-      # puts "check_for_playlist complete, playlist found"
+      end
+      puts "check_for_playlist complete, playlist not found, creating"
+      create_playlist
   end
 
   def create_playlist
@@ -102,22 +113,16 @@ class SnatchController < ApplicationController
   end
 
   def actually_snatch
-    post("users/#{session[:user_id]}/playlists/#{session[:p_id]}/tracks?uris=#{session[:s_uri]}")
-    flash[:notice] = "#{session[:s_name]} was sucsessfully added to #{session[:p_name]}"
-  end
-
-  def check_through_playlist
     playlist = get("users/#{session[:user_id]}/playlists/#{session[:p_id]}/tracks")
     for i in 0..(playlist['items'].length - 1)
       if playlist['items'][i]['track']['uri'] === session[:s_uri]
         puts "That song has already been snatched"
         flash[:alert] = "Silly goat, #{session[:s_name]} has already been snatched"
-        redirect_to root_path
         return
       end
     end
-    puts "check_through_playlist complete"
-    actually_snatch
+    post("users/#{session[:user_id]}/playlists/#{session[:p_id]}/tracks?uris=#{session[:s_uri]}")
+    flash[:notice] = "#{session[:s_name]} was sucsessfully added to #{session[:p_name]}"
   end
 
   def get(endpoint)
@@ -125,7 +130,11 @@ class SnatchController < ApplicationController
     request = Net::HTTP::Get.new(uri)
     request.content_type = "application/json"
     request["Accept"] = "application/json"
-    request["Authorization"] = "Bearer #{session[:token]}"
+    if current_user
+      request["Authorization"] = "Bearer #{current_user.access_token}"
+    else
+      request["Authorization"] = "Bearer #{session[:token]}"
+    end
     req_options = {
       use_ssl: uri.scheme == "https",
     }
@@ -140,7 +149,11 @@ class SnatchController < ApplicationController
     request = Net::HTTP::Post.new(uri)
     request.content_type = "application/json"
     request["Accept"] = "application/json"
-    request["Authorization"] = "Bearer #{session[:token]}"
+    if current_user
+      request["Authorization"] = "Bearer #{current_user.access_token}"
+    else
+      request["Authorization"] = "Bearer #{session[:token]}"
+    end
     request.body = JSON.dump body
     req_options = {
       use_ssl: uri.scheme == "https",
--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,17 @@ Rails.application.routes.draw do
   get 'snatch/about'
   get 'snatch/options'
   get 'snatch/snatch'
+  get 'snatch/fail'
+  get 'snatch/link'
+  get 'snatch/guest_snatch'
 
   root 'snatch#about'
   get 'options' => 'snatch#options'
   get 'snatch' => 'snatch#snatch'
+  get 'guest_snatch' => 'snatch#guest_snatch'
 
-  get '/auth/:provider/callback', to: 'snatch#about'
-  get '/auth/failure' , to: 'snatch#options'
+  get '/auth/:provider/callback', to: 'snatch#link'
+  get '/auth/failure' , to: 'snatch#fail'
 
   put 'snatch/options' => 'snatch#update'
 
